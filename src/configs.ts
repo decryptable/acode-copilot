44a1e1a3cc15f11c07b31b1dea0b05a300889f17
decryptable <hello@decryptable.dev>
2025-02-08 21:25:23 +0700

    Update API endpoint in configs

--- src/configs.ts
+++ src/configs.ts
@@ -2,5 +2,5 @@ export const configs = {
   // ENCRYPTION_MODE: process.env.COPILOT_ECNRYPTION_METHOD || "",
   // IV: process.env.COPILOT_IV_KEY || "",
   // PRIVATE_KEY: process.env.COPILOT_PRIVATE_KEY || "",
-  API: process.env.COPILOT_API_ENDPOINT || "",
+  API: "https://copilot.decryptable.dev/api/copilot",
 };
